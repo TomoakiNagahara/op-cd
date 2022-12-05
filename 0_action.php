ab7b067a666ec1b3bf005d483ef76becff6aaae3
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-12-06 03:27:20 +0900

    Fix: 0_action.php   | Support to each PHP version.

--- 0_action.php
+++ 0_action.php
@@ -21,7 +21,6 @@ $github_account    = $_SERVER['argv'][2] ?? null;
 $working_directory = '/www/workspace/';
 $app_root          = $working_directory . $branch . '/';
 $repository_path   = "~/repo/op/skeleton/{$branch}.git";
-$commit_id_file    = '_commit_id';
 $base_file_name    = basename($_SERVER['argv'][0]);
 
 //	Check arguments error.
@@ -55,43 +54,80 @@ if(!chdir($app_root) ){
 	exit(__LINE__);
 }
 
-//  Get commit id. <-- app-skeleton only
-$commit_id = `git show --format='%h' --no-patch`;
-
-//	Check commit id file exists.
-if( file_exists($commit_id_file) ){
-	//  Checking last commit id.
-	if( $commit_id === file_get_contents($commit_id_file) ){
-		//  Not fixed.
-		exit(0);
-	}
-}else{
-	//  Create commit id file.
-	if(!touch($commit_id_file) ){
-		exit(__LINE__);
-	}
-}
-
-//  Execute ci.php
-if( $result = `php ci.php display=1` ){
-	//	If result is error code.
-	if( strpos($result, "0\n") === 0 ){
-		ExecuteCode( explode("\n", $result) );
-	}else{
-		echo $result;
-	}
-}
-
-//	Save evaluated commit id.
-if(!file_put_contents($commit_id_file, $commit_id, LOCK_EX) ){
-	exit(__LINE__);
-}
-
-//	Push git repository to upstream.
-include('3_push.php');
+//	Execute each PHP version.
+foreach(['', 70 /*, 71, 72, 73, 74, 80, 81*/] as $php_version){ // Strict types are inconvenient.
+	//	...
+	if( ExecuteCI($php_version) ){
+		//	Push git repository to upstream.
+		include('3_push.php');
+	};
+};
 
 //	Finished
 exit(0);
+
+/** Execute CI.
+ *
+ * @created    2022-12-01
+ * @param      string     $php_version
+ * @return     boolean
+ */
+function ExecuteCI(string $php_version){
+	//	If php version is specified.
+	$branch = ($php_version === '') ? 'master': 'php'.$php_version;
+
+	//	Get branch list.
+	$branch_list = `git branch`;
+
+	//	Check if branch exists.
+	if( strpos($branch_list, $branch) === false ){
+		echo "This branch is not exists. ($branch)\n";
+		return false;
+	}
+
+	//	Switch branch.
+	$result = `git switch {$branch} 2>/dev/null`;
+
+	//	Saved commit id file name.
+	$commit_id_file = '.op-cd_commit-id.php'.$php_version;
+
+	//  Get commit id. <-- app-skeleton only
+	$commit_id = `git show --format='%h' --no-patch`;
+
+	//	Check commit id file exists.
+	if( file_exists($commit_id_file) ){
+		//  Checking last commit id.
+		if( $commit_id === file_get_contents($commit_id_file) ){
+			//  Not fixed.
+			return true;
+		}
+	}else{
+		//  Create commit id file.
+		if(!touch($commit_id_file) ){
+			echo "Failed touch({$commit_id_file}) command #".__LINE__;
+			return false;
+		}
+	}
+
+	//  Execute ci.php
+	if( $result = `php ci.php display=1` ){
+		//	If result is error code.
+		if( strpos($result, "0\n") === 0 ){
+			ExecuteCode( explode("\n", $result) );
+		}else{
+			echo $result;
+		}
+	}
+
+	//	Save evaluated commit id.
+	if(!file_put_contents($commit_id_file, $commit_id, LOCK_EX) ){
+		echo "Failed file_put_contents({$commit_id_file}) command #".__LINE__;
+		return false;
+	}
+
+	//	Return result.
+	return true;
+}
 
 /** Execute code.
  * @created    2022-11-11
@@ -107,4 +143,9 @@ function ExecuteCode($codes){
 		echo $code.PHP_EOL;
 		echo `$code`;
 	}
+}
+
+//	Eclipse Notice
+if( false ){
+	D($github_account, $app_root, $repository_path, $base_file_name);
 }
