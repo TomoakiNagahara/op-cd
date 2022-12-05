ca826398f045d6acd99979bad64f96a68a7c80ef
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-12-06 03:10:42 +0900

    Fix: 0_check.php

--- 0_check.php
+++ 0_check.php
@@ -7,3 +7,34 @@
  * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
  * @copyright Tomoaki Nagahara All right reserved.
  */
+
+/* @var $branch            string */
+/* @var $base_file_name    string */
+/* @var $github_account    string */
+/* @var $working_directory string */
+
+//	Empty branch name.
+if(!$branch ){
+	$cmd = "php {$base_file_name} [2022]";
+	echo "Empty branch name: {$cmd}\n";
+	return false;
+}
+
+//	Empty github account.
+if(!$github_account ){
+	$cmd = "php {$base_file_name} {$branch} [GITHUB PUSH ACCOUNT]";
+	echo "Empty GitHub account name: {$cmd}\n";
+	return false;
+}
+
+//	Check working directory exists.
+if(!file_exists($working_directory) ){
+	//	Create working directory.
+	if(!mkdir($working_directory, 0744, true) ){
+		echo "Create working directory is failed. ({$working_directory})\n";
+		return false;
+	}
+}
+
+//	...
+return true;
