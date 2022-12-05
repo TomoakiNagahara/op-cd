ef0da35d584ba5a23326d81b9f472537b20f7241
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-12-05 18:36:18 +0900

    Fix: 0_check.php

---
+++ 0_check.php
@@ -0,0 +1,9 @@
+<?php
+/** Arguments error check.
+ *
+ * @created   2022-12-05
+ * @version   1.0
+ * @package   op-cd
+ * @author    Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright Tomoaki Nagahara All right reserved.
+ */
