2dcd07b265ed2b53f96b9c9508c46f99edf6e756
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2022-12-07 18:03:06 +0900

    Fix: 1_clone.php    | To constant from variable.

--- 1_clone.php
+++ 1_clone.php
@@ -11,23 +11,26 @@
  * @copyright Tomoaki Nagahara All right reserved.
  */
 
+//	Return constant value.
+$_ = function($constant_name){ return $constant_name; };
+
 //	...
 $branch = Request('branch');
 
 /* @var $working_directory string */
 /* @var $repository_path   string */
 
-//	Change directory.
-if(!chdir($working_directory) ){
-	echo "Change directory is failed. ($working_directory)\n";
+//	To clarify current directory.
+if(!chdir(_WORKING_DIRECTORY_) ){
+	echo "Change directory is failed. ({$_(_WORKING_DIRECTORY_)})\n";
 	return false;
 }
 
 //	Check repository exists.
 if(!file_exists($branch) ){
 	//  Clone repository.
-	echo "\n Clone git repository - {$repository_path} \n\n";
-	`git clone -b master {$repository_path}`;
+	echo "\n Clone git repository - {$_(_REPOSITORY_PATH_)} \n\n";
+	`git clone -b master {$_(_REPOSITORY_PATH_)}`;
 }
 
 //	Check git clone success.
@@ -36,9 +39,9 @@ if(!file_exists($branch . '/.git') ){
 }
 
 //	Change directory to git repository.
-echo "\n Change directory - {$working_directory}{$branch} \n";
-if(!chdir($working_directory.$branch) ){
-	echo "Change directory failed - {$working_directory}{$branch} \n";
+echo "\n Change directory - {$_(_APP_ROOT_)} \n";
+if(!chdir(_APP_ROOT_) ){
+	echo "Change directory failed - {$_(_APP_ROOT_)} \n";
 	return false;
 }
 
