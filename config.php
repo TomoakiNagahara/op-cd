c83290fa676c05230dc2c80058cd1238a51f6cec
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-01-03 11:36:47 +0900

    NEW: config.php

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+/** op-cd:/config.php
+ *
+ * @created    2023-01-02
+ * @version    1.0
+ * @package    op-cd
+ * @author     Tomoaki Nagahara <tomoaki.nagahara@gmail.com>
+ * @copyright  Tomoaki Nagahara All right reserved.
+ */
+
+//	...
+$config = [];
+$config['workspace'] = '/www/workspace/';
+$config['branch']    = '2022';
+$config['upstream']  = 'repo:~/repo/op/skeleton/2022.git';
+$config['origin']    = '~/repo/op/skeleton/2022.git';
+
+//	...
+return $config;
