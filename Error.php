812e66118a2d75d881cb1cb5655ec5860ffca6f1
TomoakiMacBookAirM1 <tomoaki.nagahara@gmail.com>
2023-01-03 21:08:45 +0900

    Fix: Error.php | Done.

--- Error.php
+++ Error.php
@@ -10,16 +10,33 @@
 
 /** Catch standard error.
  *
- * @see   http://php.net/manual/ja/function.restore-error-handler.php
- * @param integer $errno
- * @param string  $error
- * @param string  $file
- * @param integer $line
- * @param array   $context
+ * @created    2023-01-02
+ * @see        https://www.php.net/manual/ja/function.set-error-handler.php
+ * @see        https://www.php.net/manual/ja/function.restore-error-handler.php
+ * @param      integer   $errno
+ * @param      string    $error
+ * @param      string    $file
+ * @param      integer   $line
+ * @param      array     $context is removed PHP8.0. You can receive the variables in the scope where the error occurred.
+ * @return     boolean
  */
-set_error_handler( function(...$args)
+set_error_handler( function(int $no, string $message, string $file, int $line /* , array $context */ )
 {
-	Debug($args);
+	//	...
+	echo "{$file} #{$line} - [{$no}]{$message}\n";
+	DebugTrace(debug_backtrace());
+
+	/* Can restore PHP standard error handler.
+	restore_error_handler();
+	*/
+
+	/* Can clear last error.
+	 * https://www.php.net/manual/ja/function.error-clear-last.php
+	error_clear_last();
+	*/
+
+	// If you return false, pass to PHP standard error handler.
+	return true;
 }, E_ALL);
 
 /** Catch of uncaught error.
@@ -31,7 +48,7 @@ set_exception_handler(function( \Throwable $e)
 	$file = $e->getFile();
 	$line = $e->getLine();
 	$message = $e->getMessage();
-	echo "\nException: {$file} #{$line} - {$message}\n\n";
+	echo "\nException: {$file} #{$line} - {$message}\n";
 	DebugTrace($e->getTrace());
 });
 
@@ -47,14 +64,21 @@ register_shutdown_function(function()
 	}
 
 	//	...
-	Debug($error);
-});
+	$type    = $error['type'];
+	$file    = $error['file'];
+	$line    = $error['line'];
+	$message = $error['message'];
 
+	//	...
+	echo "{$file} #{$line} - [{$type}]{$message}\n";
+	DebugTrace(debug_backtrace());
+});
 
 /** pcntl_signal
  *
  * @see https://www.php.net/manual/ja/function.pcntl-signal.php
  */
+/*
 if( function_exists('pcntl_signal') )
 {
 	// `pcntl_signal` is needs `ticks`
@@ -79,7 +103,8 @@ if( function_exists('pcntl_signal') )
 	}
 
 	//	...
-	pcntl_signal(SIGTERM, Signal);
-	pcntl_signal(SIGHUP,  Signal);
-	pcntl_signal(SIGUSR1, Signal);
+	pcntl_signal(SIGTERM, "Signal");
+	pcntl_signal(SIGHUP,  "Signal");
+	pcntl_signal(SIGUSR1, "Signal");
 }
+*/
